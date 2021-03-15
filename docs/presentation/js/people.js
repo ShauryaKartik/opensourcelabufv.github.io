a4127aaa1da9f0135ebd1d6189c401a4aff21f3f
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-03-15 09:10:47 -0300

    Members section start

---
+++ docs/presentation/js/people.js
@@ -0,0 +1,24 @@
+let members = [
+    {
+        "bio": "Meu nome é Alexandre Caldeira e sou aluno do curso de Engenharia Elétrica. Atualmente estou aprendendo sobre desenvolvimento full-stack para Tensorflow.js e sou extremamente interessado em aplicações de ciência de dados, desenvolvimento full-stack e aprendizado de máquina em soluções reais embutidas em aplicações multiplataforma, incluindo robôs.",
+        "photo": "presentation/content/members/alexandre.jpeg",
+        "social": ["https://github.com/Alexandre-Caldeira/", "https://www.linkedin.com/in/o-alexandre-caldeira/", "https://lattes.cnpq.br/1267665101394078"]
+    },
+    {
+        "bio": "Meu nome é Nicholas Barbosa e sou aluno do curso de Ciência da Computação. Atualmente estou aprendendo sobre Javascript e sou extremamente interessado em jogos, principalmente LoL",
+        "photo": "presentation/content/members/nicholas.jpeg",
+        "social": ["https://github.com/nicholasbarbosa", "https://www.linkedin.com/in/nicholas-barbosa-9200a1197/", "https://www.instagram.com/barbosanicholas/"]
+    }
+]
+
+ul = document.createElement('ul');
+
+members.forEach(member => {
+
+    let img = document.createElement('img');
+    img.src = member.photo;
+
+    img.classList.add("avatar");
+
+    document.getElementById("persons").appendChild(img);
+});
